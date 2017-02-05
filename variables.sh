2f93569a684705de4ae0f095f54b45cbb513079f
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2017-02-05 18:19:05 -0300

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -10,7 +10,7 @@ export ACCESSDTABS="$HOME/$ACCESSDTRLT"
 export HTTP=("200" "301" "400" "401" "403" "404" "500")
 export CONT="($(for((i=2000; i<5000; i++)) do echo $i; done))"
 export TIMEACCESS=("2017-01-01T" "2017-01-11T" "2017-01-06T" "2017-01-08T" "2017-01-13T" "2017-01-05T" "2017-01-15T" "2017-01-03T")
-export IPS=("50.63.202.37" "52.202.73.151" "184.168.221.2" "52.21.65.43" "8.8.8.8" "8.8.4.4" "208.67.222.222" "208.67.220.220" "84.200.69.80" "84.200.70.40")
+export IPS=("172.63.22.37" "198.20.73.151" "192.168.200.2" "152.201.65.43" "8.8.8.8" "8.8.4.4" "20.67.22.22" "108.108.22.220" "184.20.169.80" "144.250.70.40")
 export REFERERS=("https://www.99jobs.com" "https://br.linkedin.com/company" "https://web.whatsapp.com" "https://mobile.twitter.com/romuloslv")
 export RESOURCES=("/vagas" "/blog" "/empresa" "/contato" "/login" "/cases" "/anunciantes" "/in-loco-media/jobs/9748-sre-engineer-junior-trainee")
 export USERAGENTS=("Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0)"
@@ -23,7 +23,6 @@ export USERAGENTS=("Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0)"
 "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0")
 
 source $HOME/./generator.sh
-
 }
 
 Variables;
