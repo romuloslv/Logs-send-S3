e9c0b321698464aa2adf52aa3eb41c03a31382f8
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2017-06-19 21:17:39 -0300

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# For manual delete the line 4 and line 28 to 34
-:<<'manualy'
-Input ()
-{
-
-echo
-echo "Enter the number of logs to be generated!"
-echo
-echo -n :
-read NLOGS
-echo
-
-if [ $(echo $NLOGS | grep -c '^[0-9]\+$') = 0 ] || [ $NLOGS -eq 0 ]
-then
-        echo "Only positive numbers!";
-        Input;
-fi
-
-}
-
-Generator ()
-{
-
-Input;
-
-for ((i=1; i<=$NLOGS; i++))
-manualy
-
 Generator ()
 {
 
@@ -38,15 +10,11 @@ do
         "\|"\"remote_addr\": \"${IPS[$RANDOM % 10]}\", "\|"\"remote_user\": \"-\", "\|"\"body_bytes_sent\": \"133\", \
         "\|"\"request_time\": \"${CONT[$RANDOM % 3000]}\", "\|"\"status\": \"${HTTP[$RANDOM % 7]}\", "\|"\"request\": \
         \"GET /favicon.ico HTTP/1.1 ${RESOURCES[$RANDOM % 8]}\", "\|"\"request_method\": \"GET\", "\|"\"http_referrer\": \
-        \"${REFERERS[$RANDOM % 4]}\", "\|"\"http_user_agent\": \"${USERAGENTS[$RANDOM % 8]}\""" >> $HOME/$ACCESSDTRLT.log
+        \"${REFERERS[$RANDOM % 4]}\", "\|"\"http_user_agent\": \"${USERAGENTS[$RANDOM % 8]}\""" > $HOME/$ACCESSDTRLT.log.$i
 done
 
-# redundancy and scalability
-uniq $ACCESSDTRLT.log >> $HOME/log
 mkdir $ACCESSDTABS
-split -l 500 $HOME/log $ACCESSDTRLT.log.
-vector_split=($(ls $ACCESSDTRLT.log.* | cut -d'.' -f4))
-rm -f $HOME/log $ACCESSDTRLT.log
+vector_split=($(find . -name "$ACCESSDTRLT*" | cut -d'.' -f5))
 
 # treatment of ifs for json format
 for ((i=0; i<${#vector_split[*]}; i++))
@@ -55,11 +23,12 @@ do
         do
                 echo -e "{" $line | cut -d '|' -f1-500 --output-delimiter=$'\n \t'
                 echo -e "}"
-        done < $ACCESSDTRLT.log.${vector_split[$i]} >> $ACCESSDTABS/$ACCESSDTRLT.log.${vector_split[$i]}
+        done < $ACCESSDTRLT.log.${vector_split[$i]} > $ACCESSDTABS/$ACCESSDTRLT.log.${vector_split[$i]}
         rm -rf $ACCESSDTRLT.log.${vector_split[$i]}
 done
 
-source $HOME/./forward.sh
+rsync -arzhe ssh $ACCESSDTABS root@192.168.1.15:/var/log/mail
+rm -rf $ACCESSDTABS
 }
 
 Generator;
